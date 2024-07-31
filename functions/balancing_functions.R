a2069b52a683d2e55a45bc4e97ac86fb5eb23f76
noralex <ralexander.valencia@gmail.com>
2024-07-31 11:27:02 -0400

    Create balancing_functions.R

---
+++ functions/balancing_functions.R
@@ -0,0 +1,21 @@
+### Balancing functions
+
+hmin <- function(r){return(min(1,r))}
+hsq <- function(r){return(sq(r))}
+hmax <- function(r){return(max(1,r))}
+hc <- function(r,c=1){
+  first <- min(1,r*exp(-c))
+  second <- min(r,exp(-c))
+  return(max(first,second))
+}
+
+
+### Version for log probabilities
+hmin_log <- function(r){return(min(0,r))}
+hsq_log <- function(r){return(r/2)}
+hmax_log <- function(r){return(max(0,r))}
+hc_log <- function(r,c=1){
+  first <- min(1,r*exp(-c))
+  second <- min(r,exp(-c))
+  return(max(first,second))
+}
