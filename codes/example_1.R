a501d62e53b1092fc738f064b5d7f75762f9dc6b
noralex <ralexander.valencia@gmail.com>
2024-08-02 15:48:12 -0400

    adding numfer of posterior calls and distance to true F to the output

--- codes/example_1.R
+++ codes/example_1.R
@@ -1,9 +1,21 @@
 ### Source functions
+#rm(list=ls())
 source(file.path(getwd(),'functions','IITupdate.R')) #Functions for IIT update
 source(file.path(getwd(),'functions','MHupdate.R')) #Functions for MH update
 source(file.path(getwd(),'functions','MH-IITupdate.R')) #Functions for MH update
+source(file.path(getwd(),'functions','RN-IITupdate.R')) #Functions for RN-IIT update
+source(file.path(getwd(),'functions','MTM.R')) #Functions for RN-IIT update
 source(file.path(getwd(),'functions','balancing_functions.R')) #Balancing functions
 
+# num_sim=50;
+# max_iter=500*1000;
+# p=500;
+# p_1=50;
+# theta=6;
+# threshold=0.1;
+# h=hmin_log;
+# update_step=IITupdate_log;
+# name_alg='IIT'
 
 example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name_alg){
   simulation_name <- paste0('ex1_',name_alg,'_t',theta,'_p',p,'_p1_',p_1,'.csv')
@@ -13,7 +25,7 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
   #   dif <- -sum(abs(X-X_mode))
   #   return(exp(theta*dif))
   # }
-  pi <- function(X){ #Function to return the log probabilities
+  pi.distribution <- function(X){ #Function to return the log probabilities
     dif <- -sum(abs(X-X_mode))
     return(theta*dif)
   }
@@ -30,6 +42,8 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
   
   ### Need a for loop considering the number of simulations
   iter_conv <- c()
+  calls_for_pi <- c()
+  last_F_value <- c()
   for(i in 1:num_sim){
     print(paste('Simulation:',i))
     ### To check thresholds
@@ -37,10 +51,12 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
     early_finish <- FALSE #To check if convergence was achieved before max. iterations
     #Initialize
     X <- rep(0,p)
+    #Counting the number of times we used the PI function
+    count_PIs <- 0
     ### Within that for loop need a loop for the steps (considering max number of iterations)
     for(step in 1:max_iter){
       if(step %% 1000 ==0){print(paste('Iteration: ',step,', Simulation:',i))}
-      iter <- update_step(X,pi,h)
+      iter <- update_step(X,pi.distribution,h,p)
       W <- iter[[2]] #Estimated weight of previous state
       hamm_dist <- sum(abs(X_mode-X)) #distance to mode
       pi_F_est[hamm_dist + 1] <- pi_F_est[hamm_dist + 1] + W #Assign estimated weight
@@ -53,6 +69,8 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
       #If the chain has not converged
       X <- iter[[1]] #Update state
     }
+    calls_for_pi <- c(calls_for_pi,count_PIs) #Register number of times we used the PI function
+    last_F_value <- c(last_F_value,dist_pi(pi_F_est/sum(pi_F_est))) #Register last distance from true pi(F)
     #If after max_iter iterations it didn't converge
     if(!early_finish){iter_conv <- c(iter_conv,max_iter)} #register max_iter  
   }
@@ -81,6 +99,9 @@ example1(num_sim=50,
          update_step=MHupdate_log,
          name_alg='MH')
 
+MH_IIT_defined <- function(X,pi,h,p){
+  return(MH_IITupdate_log(X,pi,h,p,rho=0.025))
+}
 example1(num_sim=50,
          max_iter=500*1000,
          p=500,
@@ -88,16 +109,33 @@ example1(num_sim=50,
          theta=6,
          threshold=0.1,
          h=hmin_log,
-         update_step=MH_IITupdate_log,
+         update_step=MH_IIT_defined,
          name_alg='MH-IIT')
 
+RN_IIT_defined <- function(X,pi,h,p){
+  return(RN_IITupdate_log(X,pi,h,p,m=100))
+}
+example1(num_sim=1,
+         max_iter=500*1000,
+         p=500,
+         p_1=50,
+         theta=6,
+         threshold=0.1,
+         h=hmin_log,
+         update_step=RN_IIT_defined,
+         name_alg='RN-IIT')
 
-# num_sim=50;
-# max_iter=500*1000;
-# p=500;
-# p_1=50;
-# theta=6;
-# threshold=0.1;
-# h=hmin_log;
-# update_step=MHupdate_log;
-# name_alg='MH'
+MTM_defined <- function(X,pi,h,p){
+  return(MTMupdate_log(X,pi,h,p,m=100))
+}
+example1(num_sim=50,
+         max_iter=500*1000,
+         p=500,
+         p_1=50,
+         theta=6,
+         threshold=0.1,
+         h=hsq_log,
+         update_step=MTM_defined,
+         name_alg='MTM')
+
+
