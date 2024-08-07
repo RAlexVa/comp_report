84ac693c9bec8ffd5227ff66570a8aa18d625c75
noralex <ralexander.valencia@gmail.com>
2024-08-07 15:52:31 -0400

    final modifications to run it in the server

--- codes/example_1.R
+++ codes/example_1.R
@@ -1,5 +1,6 @@
 ### Source functions
 #rm(list=ls())
+setwd('..')
 source(file.path(getwd(),'functions','IITupdate.R')) #Functions for IIT update
 source(file.path(getwd(),'functions','MHupdate.R')) #Functions for MH update
 source(file.path(getwd(),'functions','MH-IITupdate.R')) #Functions for MH update
@@ -17,8 +18,16 @@ source(file.path(getwd(),'functions','balancing_functions.R')) #Balancing functi
 # update_step=IITupdate_log;
 # name_alg='IIT'
 
-example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name_alg){
-  simulation_name <- paste0('ex1_',name_alg,'_t',theta,'_p',p,'_p1_',p_1,'.csv')
+example1 <- function(num_sim=50,
+                     max_iter=500*1000,
+                     p=500,
+                     p_1,
+                     theta,
+                     threshold=0.1,
+                     h,
+                     update_step,
+                     name_alg){
+  simulation_name <- paste0('ex1_',name_alg,'_t',theta,'_p',p,'_p1_',p_1,'_sim',num_sim,'.csv')
   ### Call and define functions to use for the simulation
   X_mode <- c(rep(1,p_1),rep(0,p-p_1)) #Global mode for example 1
   # pi <- function(X){ #Function to return the unnormalized target pi
@@ -45,7 +54,7 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
   calls_for_pi <- c()
   last_F_value <- c()
   for(i in 1:num_sim){
-    print(paste('Simulation:',i))
+    print(paste(name_alg,'Simulation:',i,'theta',theta))
     ### To check thresholds
     pi_F_est <- numeric(p+1)
     early_finish <- FALSE #To check if convergence was achieved before max. iterations
@@ -55,9 +64,10 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
     count_PIs <- 0
     ### Within that for loop need a loop for the steps (considering max number of iterations)
     for(step in 1:max_iter){
-      if(step %% 1000 ==0){print(paste('Iteration: ',step,', Simulation:',i))}
+      if(step %% 1000 ==0){print(paste(name_alg,'theta',theta,',Iteration:',step,',Simulation:',i))}
       iter <- update_step(X,pi.distribution,h,p)
-      W <- iter[[2]] #Estimated weight of previous state
+      W <- max(iter[[2]],.Machine$double.eps) #Estimated weight of previous state, bounding it from below
+      count_PIs <- count_PIs+iter[[3]]
       hamm_dist <- sum(abs(X_mode-X)) #distance to mode
       pi_F_est[hamm_dist + 1] <- pi_F_est[hamm_dist + 1] + W #Assign estimated weight
       if(dist_pi(pi_F_est/sum(pi_F_est)) < threshold){ #Compare true dist with normalized est dist.
@@ -75,67 +85,53 @@ example1 <- function(num_sim, max_iter, p,p_1,theta,threshold,h,update_step,name
     if(!early_finish){iter_conv <- c(iter_conv,max_iter)} #register max_iter  
   }
   #iter_conv
-  write.csv(iter_conv,file=file.path(getwd(),'results',simulation_name) ,row.names = F)
+  write.csv(cbind(iter_conv,calls_for_pi,last_F_value),file=file.path(getwd(),'results',simulation_name) ,row.names = F)
 }
-
+# Parameters
+thetas <- c(6,7,8,9) 
+p1s <- c(20,50)
 set.seed(348) #Define seed
-example1(num_sim=50,
-         max_iter=500*1000,
-         p=500,
-         p_1=50,
-         theta=6,
-         threshold=0.1,
-         h=hsq_log,
-         update_step=IITupdate_log,
-         name_alg='IIT')
-
-example1(num_sim=50,
-         max_iter=500*1000,
-         p=500,
-         p_1=50,
-         theta=6,
-         threshold=0.1,
-         h=hmin_log,
-         update_step=MHupdate_log,
-         name_alg='MH')
-
-MH_IIT_defined <- function(X,pi,h,p){
-  return(MH_IITupdate_log(X,pi,h,p,rho=0.025))
+for(i in 1:length(thetas)){
+  for(j in 1:length(p1s)){
+    theta_selected <- thetas[i]
+    p1_selected <- p1s[j]
+    
+    example1(theta=theta_selected,p_1=p1_selected,
+             h=hsq_log,
+             update_step=IITupdate_log,
+             name_alg='IIT')
+    
+    example1(theta=theta_selected,p_1=p1_selected,
+             h=hmin_log,
+             update_step=MHupdate_log,
+             name_alg='MH')
+    
+    MH_IIT_defined <- function(X,pi,h,p){
+      return(MH_IITupdate_log(X,pi,h,p,rho=0.025))
+    }
+    example1(theta=theta_selected,p_1=p1_selected,
+             h=hmin_log,
+             update_step=MH_IIT_defined,
+             name_alg='MH-IIT')
+    
+    RN_IIT_defined <- function(X,pi,h,p){
+      return(RN_IITupdate_log(X,pi,h,p,m=100))
+    }
+    example1(theta=theta_selected,p_1=p1_selected,
+             h=hmin_log,
+             update_step=RN_IIT_defined,
+             name_alg='RN-IIT')
+    
+    MTM_defined <- function(X,pi,h,p){
+      return(MTMupdate_log(X,pi,h,p,m=100))
+    }
+    example1(theta=theta_selected,p_1=p1_selected,
+             h=hsq_log,
+             update_step=MTM_defined,
+             name_alg='MTM')
+  }
 }
-example1(num_sim=50,
-         max_iter=500*1000,
-         p=500,
-         p_1=50,
-         theta=6,
-         threshold=0.1,
-         h=hmin_log,
-         update_step=MH_IIT_defined,
-         name_alg='MH-IIT')
 
-RN_IIT_defined <- function(X,pi,h,p){
-  return(RN_IITupdate_log(X,pi,h,p,m=100))
-}
-example1(num_sim=1,
-         max_iter=500*1000,
-         p=500,
-         p_1=50,
-         theta=6,
-         threshold=0.1,
-         h=hmin_log,
-         update_step=RN_IIT_defined,
-         name_alg='RN-IIT')
 
-MTM_defined <- function(X,pi,h,p){
-  return(MTMupdate_log(X,pi,h,p,m=100))
-}
-example1(num_sim=50,
-         max_iter=500*1000,
-         p=500,
-         p_1=50,
-         theta=6,
-         threshold=0.1,
-         h=hsq_log,
-         update_step=MTM_defined,
-         name_alg='MTM')
 
 
