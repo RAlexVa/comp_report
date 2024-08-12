5606e0f53a0a574115d21ec699c51ad94e157f9a
noralex <ralexander.valencia@gmail.com>
2024-08-12 14:36:24 -0400

    added function for RN-IIT

--- codes/example_2.R
+++ codes/example_2.R
@@ -81,10 +81,106 @@ example2 <- function(num_sim=50,
   #iter_conv
   write.csv(cbind(iter_conv,calls_for_pi,last_F_value),file=file.path(getwd(),'results',simulation_name) ,row.names = F)
 }
+ex2_RN_IIT <- function(num_sim=50,
+                     max_iter=10*1000,
+                     p=500,
+                     theta,
+                     threshold=0.2,
+                     h,
+                     update_step=RN_IITupdate_log,
+                     m=100,
+                     name_alg){
+  simulation_name <- paste0('ex2_',name_alg,'_t',theta,'_p',p,'_sim',num_sim,'.csv')
+  ### Call and define functions to use for the simulation
+  X_mode <- c(1,rep(0,p-1)) #Global mode for example 2
+  pi.distribution <- function(X){ #Function to return the log probabilities
+    if(X[1]==1){ell <- (sum(X)-1)}
+    if(X[1]==0){ell <- 2*p-sum(X)}
+    return(-theta*ell)
+  }
+  Fm <- function(X){
+    if(X[1]==1){val <- (sum(X)-1)}
+    if(X[1]==0){val <- p}
+    return(val)
+  }
+  # To measure convergence
+  pi_F_true <- (choose(p-1,0:(p-1))*exp(-theta*(0:(p-1)))) #Vector with the true PI (pushforward measure)
+  pi_F_true <- c(pi_F_true,exp(log(1+exp(-theta))*(p-1)-theta*(p+1))) #Adding the last coordinate
+  pi_F_true <- pi_F_true/(exp(log(1+exp(-theta))*(p-1)-theta*(p+1)) + exp((p-1)*log(1+exp(-theta))))
+  
+  
+  dist_pi <- function(est){ #Function to return the TVD between the true pi and the estimated pi (pushforward measures)
+    if(length(est)!=length(pi_F_true)){
+      print('Size of vector is not the same')
+      return(1)}
+    return(sum(abs(est - pi_F_true)))
+  }
+  
+  ### Need a for loop considering the number of simulations
+  iter_conv <- c()
+  calls_for_pi <- c()
+  last_F_value <- c()
+  for(i in 1:num_sim){
+    print(paste(name_alg,'Simulation:',i,'theta',theta))
+    ### To check thresholds
+    pi_F_est <- numeric(p+1)
+    early_finish <- FALSE #To check if convergence was achieved before max. iterations
+    #Initialize
+    X <- c(rep(0,10),rep(1,p-10))
+    #Counting the number of times we used the PI function
+    count_PIs <- 0
+    #Initial neighborhood
+    neighbors <- sample(1:p,m, replace = F) #Choose m neighbors
+    ### Within that for loop need a loop for the steps (considering max number of iterations)
+    for(step in 1:max_iter){
+      if(step %% 1000 ==0){print(paste(name_alg,'theta',theta,',Iteration:',step,',Simulation:',i))}
+      iter <- update_step(X,pi.distribution,h,p,m,neighbors)
+      W <- max(iter[[2]],.Machine$double.eps) #Estimated weight of previous state, bounding it from below
+      count_PIs <- count_PIs+iter[[3]]
+      #Adding a minimum that is above 0 to avoid issues with very low numbers
+      Feval <- Fm(X) +1 #Function F evaluated
+      pi_F_est[Feval] <- pi_F_est[Feval] + W #Assign estimated weight
+      
+      if(dist_pi(pi_F_est/sum(pi_F_est)) < threshold){ #Compare true dist with normalized est dist.
+        #If the chain converged
+        iter_conv <- c(iter_conv,step) #Register number of steps needed
+        early_finish <- TRUE
+        break; #Finish this simulation
+      }
+      #If the chain has not converged
+      X <- iter[[1]] #Update state
+      neighbors <- iter[[4]] #Update neighborhood
+    }
+    calls_for_pi <- c(calls_for_pi,count_PIs) #Register number of times we used the PI function
+    last_F_value <- c(last_F_value,dist_pi(pi_F_est/sum(pi_F_est))) #Register last distance from true pi(F)
+    #If after max_iter iterations it didn't converge
+    if(!early_finish){iter_conv <- c(iter_conv,max_iter)} #register max_iter  
+  }
+  #iter_conv
+  write.csv(cbind(iter_conv,calls_for_pi,last_F_value),file=file.path(getwd(),'results',simulation_name) ,row.names = F)
+}
+
+
+MH_IIT_defined <- function(X,pi,h,p){
+  return(MH_IITupdate_log(X,pi,h,p,rho=0.025))
+}
+theta_selected <- 6
+h_defined <- function(r){return(hc_log(r,c=2*theta_selected))}
+
+num_sim=1;
+max_iter=10*1000;
+p=500;
+theta=6;
+threshold=0.2;
+h=h_defined;
+update_step=MH_IIT_defined;
+name_alg='MH_IIT'
+
+
 
 # Parameters
 thetas <- c(6,7,8,9) 
-set.seed(235) #Define seed
+set.seed(1235) #Define seed
 for(i in 1:length(thetas)){
     theta_selected <- thetas[i]
     
@@ -106,21 +202,15 @@ for(i in 1:length(thetas)){
              update_step=MH_IIT_defined,
              name_alg='MH-IIT-1')
     
-    MH_IIT_defined <- function(X,pi,h,p){
-      return(MH_IITupdate_log(X,pi,h,p,rho=0.025))
-    }
     h_defined <- function(r){return(hc_log(r,c=2*theta_selected))}
     example2(theta=theta_selected,
              h=h_defined,
              update_step=MH_IIT_defined,
              name_alg='MH-IIT-2')
     
-    RN_IIT_defined <- function(X,pi,h,p){
-      return(RN_IITupdate_log(X,pi,h,p,m=100))
-    }
-    example2(theta=theta_selected,
+    ex2_RN_IIT(theta=theta_selected,
              h=hmin_log,
-             update_step=RN_IIT_defined,
+             update_step=RN_IITupdate_log,
              name_alg='RN-IIT')
     
     MTM_defined <- function(X,pi,h,p){
