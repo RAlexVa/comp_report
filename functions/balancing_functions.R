d74ad6e37e5893fac4e895d0077ebb7e64444783
noralex <ralexander.valencia@gmail.com>
2024-08-12 14:36:02 -0400

    fix hc balancing function to work properly with log probabilities

--- functions/balancing_functions.R
+++ functions/balancing_functions.R
@@ -15,7 +15,7 @@ hmin_log <- function(r){return(min(0,r))}
 hsq_log <- function(r){return(r/2)}
 hmax_log <- function(r){return(max(0,r))}
 hc_log <- function(r,c=1){
-  first <- min(1,r*exp(-c))
-  second <- min(r,exp(-c))
+  first <- min(0,r-c)
+  second <- min(r,-c)
   return(max(first,second))
 }
