dcb5974a8a5fa15e362aca69711895e46ced06cc
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-10-07 10:21:14 -0400

    update functions of Rcpp

--- codes/random_normal.cpp
+++ codes/random_normal.cpp
@@ -3,29 +3,9 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 using namespace arma;
-// [[Rcpp::export]]
-mat random_model_v1(int n, int p) {
-  mat ranmat(n,p);
-  vec Y(n);
-  vec beta;
-  // Define beta
-  beta = Rcpp::runif(1);
-  beta = (4+(beta*2))*sqrt(log(p)/n);
-    for(int j=0; j<p;j++){
-      ranmat.col(j) = as<vec>(Rcpp::rnorm(n));
-    }
-// Define response
-    Y= ((ranmat.col(0) + ranmat.col(1) + ranmat.col(2))*beta) + as<vec>(Rcpp::rnorm(n));
-    
-//Create multimodality
 
-ranmat.col(3) = ranmat.col(1)-ranmat.col(2)+as<vec>(Rcpp::rnorm(n));
-ranmat.col(4) = ranmat.col(0)+ranmat.col(1)+ranmat.col(2)+ranmat.col(5)+ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
-ranmat.col(7) = ranmat.col(5)-ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
-
-return ranmat;
-}
-
+//Given dimensions, return a random model 
+//following the description provided in the IIT paper
 // [[Rcpp::export]]
 List random_model(int n, int p) {
 // Create the model matrix
@@ -53,6 +33,9 @@ List random_model(int n, int p) {
   return ret;
 }
 
+
+// Given a model matrix, a response vector and a vector of positions
+//return the likelihood of the model using the specified variables
 // [[Rcpp::export]]
 double logLikelihood(mat X, colvec Y, uvec pos){
   mat subX = X.cols(pos);
@@ -70,7 +53,7 @@ return logl;
 }
 
 /*** R
-logLikelihood(res$X,res$Y,c(1:3,9:19)-1)
+#logLikelihood(res$X,res$Y,c(1:3,9:19)-1)
 # set.seed(123)
 # m <- random_norm(5,4)
 # set.seed(123)
