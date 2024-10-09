57143be185f6d0f30e16b6b0767cd49d22dd4b10
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-10-09 11:25:02 -0400

    adding logpsi factors to the temporal weight
    we also removed the R code from this file

--- functions/cpp_functions.cpp
+++ functions/cpp_functions.cpp
@@ -72,7 +72,7 @@ double logL_0(colvec Y){
 //Below function works with loglikelihood
 // This is the update for method 1
 // [[Rcpp::export]]
-List VT_IIT_update_c1(colvec X, vec temp, uvec curr_temp, mat modelX, vec resY, int n, int t) {
+List VT_IIT_update_c1(colvec X, vec temp, uvec curr_temp, mat modelX, vec resY, int n, int t, vec logpsi) {
   double n_double = double(n);
   double t_double = double(t);
   // Create the model matrix
@@ -119,7 +119,7 @@ double temporal=0;
    //Usually we deleted the entry for the current temperature
    //But it's easier just to assign a probability 0 to choose it
    if(temp_nei==temperature){probs(j)=0;}else{//
-     temporal=(logpi_current*(temp_nei-temperature));
+     temporal=(logpi_current*(temp_nei-temperature) + conv_to<double>::from(logpsi.row(j-n))-conv_to<double>::from(temp.elem(curr_temp)));
      if(temporal<0){probs(j) = (exp(temporal)/(t_double-1));}else{probs(j)=(1/(t_double-1));}}
  }
  
@@ -154,7 +154,7 @@ vec update_logpsi(vec logpsi, int curr_temp, double iteration, int J){
   double n_0=100;
   double s_0=100;
   double temporal = conv_to<double>::from(logpsi.row(curr_temp)) - s_0/(n_0+iteration);
-  logpsi = logpsi + s_0/((J-1)*(n_0 + iteration));
+  logpsi = logpsi + s_0/(J*(n_0 + iteration));
   logpsi.row(curr_temp) = temporal;
   return(logpsi);
 }
@@ -168,7 +168,7 @@ vec Simulation_mod1(int n, int p, int numsim, int numiter, vec temp,int t){
 for(int s=0; s<numsim;s++){
   //Rcpp::Rcout << "Starts simulation number  "<< s<<std::endl;
   // Creates model
-  // Create the model matrix
+  // Create the model matrix X and response Y
   mat modelX(n,p);
   vec resY(n);
   vec beta;
@@ -188,11 +188,14 @@ for(int s=0; s<numsim;s++){
  // Rcpp::Rcout << "Model defined  "<< modelX<<std::endl; 
  // Rcpp::Rcout << "Model defined  "<< resY<<std::endl; 
 // Here we have the model defined for iteration #s
-//The we start the for loop to run over iterations
+
+///////////////////////////////////////////////////////////////////////////
+//Then we start the for loop to run over iterations
 vec X(p,fill::zeros); // The starting state of all simulations is a vector full of zeroes
 // Rcpp::Rcout << "Creates vector of 0  "<< X<<std::endl;
 uvec curr_temp = "0"; // We start in the very first temperature (which should be 1)
 // Rcpp::Rcout << "Defines initial temperature "<< curr_temp<<std::endl;
+vec logpsi(t, fill::zeros); // Initialize vector logpsi, as many entries as temperatures
 //Starts simulation
 for(int i=0;i<numiter;i++){
   // Rcpp::Rcout << "Starts iteration  "<< i<<std::endl; 
@@ -268,123 +271,6 @@ for(int i=0;i<numiter;i++){
 
 return temp;
 }//End of function
-
-
-/*** R
-#rm(list=ls())
-n <- 10
-p <- 15
-#res <- random_model(n,p)
-set.seed(134)
-temp <- c(1,1.2,2.3,3.5,3.9,4)
-Simulation_mod1(n=n,p=p,numsim=2,numiter=2,temp=temp,t=length(temp))
-
-
-# X <- c(0,1,0,rep(0,12))
-# 
-# curr_temp <- 3
-# set.seed(134)
-# (check <- VT_IIT_update_c1(X=X,
-#                 temp=temp,
-#                 curr_temp=curr_temp,
-#                 modelX=res$X,
-#                 resY=res$Y,
-#                 n=length(X),
-#                 t=length(temp))) #number of temperatures
-# 
-# update_logpsi(c(1,2,3,4,5),2,10,5)
-
-# library(dplyr)
-# set.seed(134)
-# temperature <- temp[curr_temp+1]
-# coord <- which(X==1)+1
-# t <- length(temp)
-# logpi_r <- c(p+t)
-# data <- as.data.frame(cbind(res$Y,res$X))
-# logpi_current <- logLik(lm(V1 ~.-1, data=as.data.frame(data[,c(1,coord)])))[1]
-# for(i in 1:p){
-#   Xnew <- X;
-#   Xnew[i] <- 1-X[i];
-#   variables <- which(Xnew==1);
-#   if(length(variables)==0){
-#     data_s <- data |>  select('V1');
-#     mod <- lm(V1 ~., data=data_s)
-#   }else{
-#     data_s <- data[,c(1,variables+1)] #Include column 1 since it's Y, shift indexes to consider column Y
-#     mod <- lm(V1 ~.-1, data=data_s)
-#   }
-# logpi_r[i] <- exp(min((logLik(mod)[1] - logpi_current)*temperature,0))/length(X)
-# }
-# for(i in 1:t){
-#   if(temp[i]==temperature){logpi_r[p+i] <- 0}else{
-#   logpi_r[p+i] <- exp(min(logpi_current*(temp[i]-temperature),0))/(length(temp)-1)
-# }}
-# logpi_r
-# u <- runif(p+t)
-# which.min(-log(u)/logpi_r)
-
-
-### Compare
-# fun_dummy_1 <- function(X=X,
-#                         temp=temp,
-#                         curr_temp=curr_temp,
-#                         modelX=res$X,
-#                         resY=res$Y,
-#                         n=length(X),
-#                         t=length(temp)){
-#   return(VT_IIT_update_c1(X=X,
-#                           temp=temp,
-#                           curr_temp=curr_temp,
-#                           modelX=res$X,
-#                           resY=res$Y,
-#                           n=length(X),
-#                           t=length(temp)))
-# }
-# 
-# fun_dummy_2 <- function(X=X,
-#                         temp=temp,
-#                         curr_temp=curr_temp,
-#                         modelX=res$X,
-#                         resY=res$Y,
-#                         n=length(X),
-#                         t=length(temp)){
-#   temperature <- temp[curr_temp+1]
-#   coord <- which(X==1)+1
-#   t <- length(temp)
-#   logpi_r <- c(p+t)
-#   data <- as.data.frame(cbind(res$Y,res$X))
-#   logpi_current <- logLik(lm(V1 ~.-1, data=as.data.frame(data[,c(1,coord)])))[1]
-#   for(i in 1:p){
-#     Xnew <- X;
-#     Xnew[i] <- 1-X[i];
-#     variables <- which(Xnew==1);
-#     if(length(variables)==0){
-#       data_s <- data |>  select('V1');
-#       mod <- lm(V1 ~., data=data_s)
-#     }else{
-#       data_s <- data[,c(1,variables+1)] #Include column 1 since it's Y, shift indexes to consider column Y
-#       mod <- lm(V1 ~.-1, data=data_s)
-#     }
-#     logpi_r[i] <- exp(min((logLik(mod)[1] - logpi_current)*temperature,0))/length(X)
-#   }
-#   for(i in 1:t){
-#     if(temp[i]==temperature){logpi_r[p+i] <- 0}else{
-#       logpi_r[p+i] <- exp(min(logpi_current*(temp[i]-temperature),0))/(length(temp)-1)
-#     }}
-#   logpi_r
-#   
-#   u <- runif(p+t)
-#   
-#   index_n <- which.min(-log(u)/logpi_r)
-#   if(index_n<=p){X[index_n] <- 1-X[index_n]}else{
-#     temperature = temp[index_n-p]
-#   }
-#   
-#   return(list(temperature,logpi_r,X,))
-# }
-# 
-# microbenchmark(fun_dummy_1, fun_dummy_2, times=1000)
-*/
 
 // How to use functions inside other functions
 //If they're in the same file, they can interact
