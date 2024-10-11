430f95ea06914240541b0a225b1f46250712eed1
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-10-11 16:41:21 -0400

    cpp function only with random_model

---
+++ functions/cpp_func_random_model.cpp
@@ -0,0 +1,32 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+using namespace arma;
+//Given dimensions, return a random model 
+//following the description provided in the IIT paper for testing algorihtm VT-IIT
+// [[Rcpp::export]]
+List random_model(int n, int p) {
+  // Create the model matrix
+  mat ranmat(n,p);
+  vec Y(n);
+  vec beta;
+  // Define beta
+  beta = Rcpp::runif(1);
+  beta = (4+(beta*2))*sqrt(log(p)/n);
+  for(int j=0; j<p;j++){
+    ranmat.col(j) = as<vec>(Rcpp::rnorm(n));
+  }
+  // Define response
+  Y= ((ranmat.col(0) + ranmat.col(1) + ranmat.col(2))*beta) + as<vec>(Rcpp::rnorm(n));
+  
+  //Create multimodality
+  
+  ranmat.col(3) = ranmat.col(1)-ranmat.col(2)+as<vec>(Rcpp::rnorm(n));
+  ranmat.col(4) = ranmat.col(0)+ranmat.col(1)+ranmat.col(2)+ranmat.col(5)+ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
+  ranmat.col(7) = ranmat.col(5)-ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
+  
+  List ret;
+  ret["Y"]=Y;
+  ret["X"]=ranmat;
+  return ret;
+}
