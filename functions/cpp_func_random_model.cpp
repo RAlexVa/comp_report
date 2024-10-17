b810a0071370bbd555c7aab527386743d262ba54
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-10-17 13:40:27 -0400

    modifying SD of the noise
    modify the model definition to reduce the sd of the noise according to the specified in IIT paper

--- functions/cpp_func_random_model.cpp
+++ functions/cpp_func_random_model.cpp
@@ -14,16 +14,16 @@ List random_model(int n, int p) {
   beta = Rcpp::runif(1);
   beta = (4+(beta*2))*sqrt(log(p)/n);
   for(int j=0; j<p;j++){
-    ranmat.col(j) = as<vec>(Rcpp::rnorm(n));
+    ranmat.col(j) = as<vec>(Rcpp::rnorm(n,0.0,1.0));//mean 0, standard deviation 1
   }
   // Define response
-  Y= ((ranmat.col(0) + ranmat.col(1) + ranmat.col(2))*beta) + as<vec>(Rcpp::rnorm(n));
+  Y= ((ranmat.col(0) + ranmat.col(1) + ranmat.col(2))*beta) + as<vec>(Rcpp::rnorm(n,0.0,0.5));
   
   //Create multimodality
   
-  ranmat.col(3) = ranmat.col(1)-ranmat.col(2)+as<vec>(Rcpp::rnorm(n));
-  ranmat.col(4) = ranmat.col(0)+ranmat.col(1)+ranmat.col(2)+ranmat.col(5)+ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
-  ranmat.col(7) = ranmat.col(5)-ranmat.col(6)+as<vec>(Rcpp::rnorm(n));
+  ranmat.col(3) = ranmat.col(1)-ranmat.col(2)+as<vec>(Rcpp::rnorm(n,0.0,0.1));
+  ranmat.col(4) = ranmat.col(0)+ranmat.col(1)+ranmat.col(2)+ranmat.col(5)+ranmat.col(6)+as<vec>(Rcpp::rnorm(n,0.0,0.1));
+  ranmat.col(7) = ranmat.col(5)-ranmat.col(6)+as<vec>(Rcpp::rnorm(n,0.0,0.1));
   
   List ret;
   ret["Y"]=Y;
