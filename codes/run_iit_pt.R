4c71714953a7d8ad6d48e10fdc839a4303e33058
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-10-27 18:30:10 -0400

    fixing file naming

--- codes/run_iit_pt.R
+++ codes/run_iit_pt.R
@@ -39,7 +39,7 @@ for(chunk_selected in 1:5){
   set.seed(seed_def + chunk_selected)
   results <- RF_PT_IIT_sim(p=p,startsim=start_point, endsim=end_point, numiter=iterations, iterswap=100, temp=temp, method=m_selected)
   write.table(results$modes,paste0('results/','resultados_PT-IIT_modelo',m_selected,'_temp_',t_selected,'_seed_',seed_def,'+',chunk_selected,'sim',start_point,'_',end_point,'_modes','.csv'),row.names=F, col.names=F, sep=',')  
-  write.table(results$ip,paste0('results/','resultados_VT-IIT_modelo',m_selected,'_temp_',t_selected,'_seed_',seed_def,'+',chunk_selected,'sim',start_point,'_',end_point,'_ip','.csv'),row.names=F, col.names=F, sep=',')  
+  write.table(results$ip,paste0('results/','resultados_PT-IIT_modelo',m_selected,'_temp_',t_selected,'_seed_',seed_def,'+',chunk_selected,'sim',start_point,'_',end_point,'_ip','.csv'),row.names=F, col.names=F, sep=',')  
 }
 
 
